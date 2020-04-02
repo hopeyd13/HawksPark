032db78d4920d394628ed7b1d1c22270f57789b8
Hope Davis <s1072896@monmouth.edu>
2020-04-02 14:19:27 -0400

    server change

--- nodejs-express-mysql/server.js
+++ nodejs-express-mysql/server.js
@@ -17,6 +17,6 @@ app.get("/", (req, res) => {
 require("./app/routes/space.routes.js")(app);
 
 // set port, listen for requests
-app.listen(3000, () => {
-  console.log("Server is running on port 3000.");
+app.listen(22, () => {
+  console.log("Server is running on port 22.");
 });
