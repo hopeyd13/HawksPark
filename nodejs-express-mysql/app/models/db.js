1e238b56bbe607890f4b7eb3e435a02012e80ad8
Hope Davis <s1072896@monmouth.edu>
2020-04-02 15:03:54 -0400

    connection edit

--- nodejs-express-mysql/app/models/db.js
+++ nodejs-express-mysql/app/models/db.js
@@ -1,12 +1,12 @@
 const mysql = require("mysql");
-const dbConfig = require("../config/db.config.js");
+//const dbConfig = require("../config/db.config.js");
 
 // Create a connection to the database
 const connection = mysql.createConnection({
-  host: dbConfig.HOST,
-  user: dbConfig.USER,
-  password: dbConfig.PASSWORD,
-  database: dbConfig.DB
+  host: process.env.RDS_HOSTNAME,
+  user: process.env.RDS_USERNAME,
+  password: process.env.RDS_PASSWORD,
+  port: process.env.RDS_PORT
 });
 
 // open the MySQL connection
