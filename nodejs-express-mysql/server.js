7341a353d2f5aa3344a4bb3356612f65d55460f5
Hope Davis <s1072896@monmouth.edu>
2020-04-03 17:36:13 -0400

    test

--- nodejs-express-mysql/server.js
+++ nodejs-express-mysql/server.js
@@ -7,12 +7,12 @@ const mysql = require("mysql");
 //const dbConfig = require("../config/db.config.js");
 
 // Create a connection to the database
-const connection = mysql.createConnection({
-  host: process.env.RDS_HOSTNAME,
-  user: process.env.RDS_USERNAME,
-  password: process.env.RDS_PASSWORD,
-  port: process.env.RDS_PORT
-});
+// const connection = mysql.createConnection({
+//   host: process.env.RDS_HOSTNAME,
+//   user: process.env.RDS_USERNAME,
+//   password: process.env.RDS_PASSWORD,
+//   port: process.env.RDS_PORT
+// });
 
 // const connection = mysql.createConnection({
 //   host: dbConfig.HOST,
