d5429dfa64e8c66e92f2af17f3f6afb194258a38
Hope Davis <s1072896@monmouth.edu>
2020-04-03 12:48:05 -0400

    another try

--- nodejs-express-mysql/app/config/db.config.js
+++ nodejs-express-mysql/app/config/db.config.js
@@ -2,6 +2,6 @@ module.exports = {
     HOST: "ec2-52-90-111-121.compute-1.amazonaws.com",
     USER: "ec2-user",
     PASSWORD: "csseHH219!",
-    PORT: 3306,
+    PORT: '3306',
     DB: "aadlp21x2bofu7"
 };
