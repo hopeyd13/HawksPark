1ab5405c81b765079462c82b79de204e02f16a52
Hope Davis <s1072896@monmouth.edu>
2020-04-03 13:42:57 -0400

    troubleshooting cdb conn

--- nodejs-express-mysql/app/config/db.config.js
+++ nodejs-express-mysql/app/config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     HOST: "ec2-52-90-111-121.compute-1.amazonaws.com",
-    USER: "ec2-user",
+    USER: "admin",
     PASSWORD: "csseHH219!",
     PORT: "3306",
     DB: "aadlp21x2bofu7"
